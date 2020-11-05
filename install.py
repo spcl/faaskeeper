fcf7c9f4d9a69d043521db6d442866ac38075ca1
mcopik <mcopik@gmail.com>
2020-11-05 01:16:43 +0100

    Improve install script

--- install.py
+++ install.py
@@ -17,20 +17,19 @@ def execute(cmd):
         )
     return ret.stdout.decode("utf-8")
 
-env_dir="python-venv"
+python_env_dir="python-venv"
 
-print("Creating Python virtualenv at {}".format(env_dir))
-execute("python3 -mvenv {}".format(env_dir))
+print("Creating Python virtualenv at {}".format(python_env_dir))
+execute("python3 -mvenv {}".format(python_env_dir))
 
 print("Install Python dependencies with pip")
-execute(". {}/bin/activate && pip3 install -r requirements.txt".format(env_dir))
+execute(". {}/bin/activate && pip3 install -r requirements.txt".format(python_env_dir))
 
 print("Configure mypy extensions")
-execute(". {}/bin/activate && mypy_boto3".format(env_dir))
+execute(". {}/bin/activate && mypy_boto3".format(python_env_dir))
 
-env_dir="node-venv"
-print("Creating Node virtualenv at {}".format(env_dir))
-execute("nodeenv {}".format(env_dir))
+print("Creating Node virtualenv at {}".format(node_env_dir))
+execute(". {}/bin/activate && nodeenv -p".format(python_env_dir))
 print("Install Node dependencies with npm")
-execute(". {}/bin/activate && npm install install".format(env_dir))
+execute(". {}/bin/activate && npm install -g".format(python_env_dir))
 
