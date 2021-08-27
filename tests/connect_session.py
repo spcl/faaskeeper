66d117616d79311376ff2c286b1afc87b2b268ad
Marcin Copik <mcopik@gmail.com>
2021-08-27 16:48:30 +0200

    Add first test for session registration

---
+++ tests/connect_session.py
@@ -0,0 +1,43 @@
+import os
+
+import pytest
+
+from faaskeeper.client import FaaSKeeperClient
+from faaskeeper.exceptions import FaaSKeeperException
+
+SERVICE_NAME = os.environ.get("FK_TEST_SERVICE_NAME")
+SERVICE_PORT = int(os.environ.get("FK_TEST_SERVICE_PORT"))
+
+
+@pytest.fixture
+def aws_connect():
+    return FaaSKeeperClient(
+        "aws", service_name=SERVICE_NAME, port=SERVICE_PORT, verbose=False
+    )
+
+
+@pytest.mark.parametrize("client", ["aws_connect"])
+def test_connection(client, request):
+
+    client = request.getfixturevalue(client)
+
+    try:
+        client.start()
+        assert client.session_id
+        assert client.session_status == "CONNECTED"
+    except FaaSKeeperException as e:
+        pytest.fail(f"Unexpected FaaSKeeperException exception {e}")
+    except Exception as e:
+        pytest.fail(f"Unexpected general exception {e}")
+
+    try:
+        client.stop()
+        assert not client.session_id
+        assert client.session_status == "DISCONNECTED"
+    except FaaSKeeperException as e:
+        pytest.fail(f"Unexpected FaaSKeeperException exception {e}")
+    except Exception as e:
+        pytest.fail(f"Unexpected general exception {e}")
+
+
+#  reconnect after
