158324777a8477725afa900d1652c20c96d26e4b
Marcin Copik <mcopik@gmail.com>
2021-08-27 16:48:16 +0200

    Add isort

--- tools/linting.py
+++ tools/linting.py
@@ -10,6 +10,9 @@ arg = argv[1]
 print("Code formatting of with Black")
 call("black", arg, "--config .black.toml")
 
+print("Import sorting with isort")
+call("isort", arg, "-p faaskeeper -l 88")
+
 print("flake8 linting")
 call("flake8", arg, "--config=.flake8.cfg --black-config=.black.toml")
 
